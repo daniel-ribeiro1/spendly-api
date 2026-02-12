dfa062235788432293b39a3fbe78c1b1e3eab9c0
Daniel R. Oliveira <daniel.ribeiro.dev@outlook.com>
2026-02-12 14:51:09 -0300

    feat: criar o endpoint de health check

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.API_PORT ?? 3000);
+
+  Logger.log(`API is running on port ${process.env.API_PORT ?? 3000}`, 'Main');
 }
+
 bootstrap();
